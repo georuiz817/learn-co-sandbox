22d32abe5230d2e24994933c14ace7dcd6052f3e
George Ruiz <georuiz817@gmail.com>
2019-07-09 20:35:47 +0000

    additional mock code w/ added pokemon list

---
+++ poke_dex/lib/poke_dex/cli.rb
@@ -0,0 +1,44 @@
+class PokeDex::CLI
+  
+ def call
+   start
+ end
+  
+  
+  def start 
+    puts "Welcome to the Pokedex! Type menu to view the encyclopedia of Pokemon"
+    input = gets.strip
+    if input == "menu"
+      puts ""
+     menu
+   elsif input == "quit"
+   quit 
+   else
+     puts "I did not understand that command"
+      puts " "
+      call
+  end
+end
+
+
+  
+  def menu
+  puts <<~DOC 
+  1. Bulbasaur 
+  2. Pikachu 
+  3. Shuckle 
+  4. Rolycoly
+  5. Mareep 
+  DOC
+  puts "Trainer, choose the Pokemon you'd like get more information on or press exit to quit the Pokedex."
+  end
+
+
+
+def quit 
+  puts ""
+  puts "Thanks for using the Pokedex!"
+end
+
+
+end
